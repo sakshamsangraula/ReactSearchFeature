0f1c10302887ad183161163a17b61a2197033b9a
sakshamsangraula <55895550+sakshamsangraula@users.noreply.github.com>
2020-01-11 23:50:44 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCIPsLz_r0-aTeNzXxsg5PSBUhZ_R7Bvoo';
+// create a new component that produces HTML
+
+const App = () => {
+   return <div><SearchBar /></div> 
+}
+
+// Put the generated HTML into the page (in the dom)
+
+// render the App component to dom by using ReactDom
+
+// Instantiate components before rendering them to the Dom
+// by creating an instance of App (<App/>) and put that instance inside index.html(inside a div)
+// so the ReactDom knows where to render the instance
+ReactDom.render(<App />, document.querySelector('.container'));
